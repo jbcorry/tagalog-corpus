3aa8fb33c5c1efbb926d564ddd06b5666bfb8ba7
Jake <jakecorry123@gmail.com>
2020-04-17 07:52:48 -0600

    clean up and add css

--- tagcorp/urls.py
+++ tagcorp/urls.py
@@ -6,12 +6,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('search_word', views.search_word, name='search_word'),
     path('pop_database/', views.pop_database, name='pop_database'),
-    path('signin', views.signIn, name='signin'),
-    path('postsign/', views.postsign, name='signin'),
-    path('logout/', views.logout, name='logout'),
-    path('signup/', views.signUp, name='signup'),
-    path('postsignup/', views.postsignup, name='postsignup'),
-    path('create/', views.create, name='create'),
-    path('postcreate/', views.postcreate, name='postcreate'),
-    path('check/', views.check, name='check'),
 ]
